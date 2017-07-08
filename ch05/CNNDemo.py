152e8b84bd97f8e29fdb28216977a11929f5469d
mokundong <mokundong@sina.com>
2017-07-08 23:33:44 +0800

    MKD

--- ch05/CNNDemo.py
+++ ch05/CNNDemo.py
@@ -4,13 +4,64 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
 sess = tf.InteractiveSession()
 #定义权重
-def weught_variable(shape):
+def weight_variable(shape):
     initial = tf.truncated_normal(shape,stddev=0.1)#分布类型，标准差0.1
     return tf.Variable(initial)
 #定义偏置
 def bias_variable(shape):
-    initial = tf.concat(0.1,shape)#添加正值0.1避免死亡节点
+    initial = tf.constant(0.1,shape=shape)#添加正值0.1避免死亡节点
     return tf.Variable(initial)
 #创建2维卷积函数
-def  conv2d(x,W):#x:输入，W是卷积参数
+def  conv2d(x,W):#x:输入，W是卷积参数，比如[5,5,1,32]头两个表示卷积核尺寸，第三个数表示有多少channel，最后一个数字表示卷积核的数量
     return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding='SAME')#serides代表卷积模板移动的步长，都是1代表不遗漏的划过每一个点
+#最大池化函数
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+#定义placeholder
+#x:特征
+x = tf.placeholder(tf.float32,[None,784])#784表示1D输入向量
+#_y：真实的label
+y_ = tf.placeholder(tf.float32,[None,10])
+#将1x784转换成28*28
+x_image = tf.reshape(x,[-1,28,28,1])#-1：表示样本数量不固定，28,28：表示2D结构，1：代表颜色通道数量
+#定义第一个卷积层
+W_conv1 = weight_variable([5,5,1,32])#定义Weights，[5,5,1,25]表示卷积核尺寸5x5，一个颜色通道，32个不同的卷积核
+b_conv1 = bias_variable([32])#
+h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1)+b_conv1)#卷积操作并加上偏置,然后利用relu激活函数进行非线性处理
+h_pool1 = max_pool_2x2(h_conv1)
+#定义第二个卷积层
+W_conv2 = weight_variable([5,5,32,64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2)+b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+#
+W_fc1 = weight_variable([7*7*64,1024])#两次2X2的最大池化，变成只有原来1/4，尺寸由28*28变成了7*7，第二个卷积层的个个数为64，输出tensor为7*7*64,建立一个全连接，隐含节点1024
+b_fc1 = bias_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])#变形成为1D的向量
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
+#为了减轻过拟合，使用一个Dropout层
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
+#将Dropout层输出连接一个Softmax层，得到最后的概率输出
+W_fc2 = weight_variable([1024,10])
+b_fc2 = bias_variable([10])
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop,W_fc2)+b_fc2)
+#定义损失函数为cross entropy，使用Adam优化器，并给予一个比较小的学习速率1e-4
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv),reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+#定义评测准确率的操作
+correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+#开始训练
+#训练时Dropout的keep_prob比率为0.5，然后使用大小为50的mini-batch，
+# 共进行20000次训练迭代，参与训练的样本数量总共为100万
+#其中每100次训练，对准确率进行一次评测（评测时keep_prob设为1）
+tf.global_variables_initializer().run()#初始化所有参数
+for i in range(20000):
+    batch = mnist.train.next_batch(50)
+    if i%100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1],keep_prob:1.0})
+        print("step %d,training accuary %g"%(i,train_accuracy))
+    train_step.run(feed_dict={x:batch[0],y_:batch[1],keep_prob:0.5})
+#测试
+print("test accuracy %g"%accuracy.eval(feed_dict={x:mnist.test.images,y_:mnist.test.labels,keep_prob:1.0}))
